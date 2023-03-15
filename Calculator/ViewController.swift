2b656b876c64b365ddc625937f8293b900f16185
Oleksii Moroz <blackswift1992@gmail.com>
2023-03-15 19:53:53 +0200

    show on displayLabel which numButton was pressed

--- Calculator/ViewController.swift
+++ Calculator/ViewController.swift
@@ -5,18 +5,27 @@ class ViewController: UIViewController {
     
     @IBOutlet weak var displayLabel: UILabel!
     
+    var isFinishedTypingNumber = true
     
     
     @IBAction func calcButtonPressed(_ sender: UIButton) {
-        
-        //What should happen when a non-number button is pressed
+        isFinishedTypingNumber = true
     
     }
 
     
     @IBAction func numButtonPressed(_ sender: UIButton) {
-        
-        //What should happen when a number is entered into the keypad
+        if let numButtonValue = sender.currentTitle,
+           let displayLabelValue = displayLabel.text {
+            if isFinishedTypingNumber {
+                displayLabel.text = numButtonValue
+                isFinishedTypingNumber = false
+            } else {
+                displayLabel.text = displayLabelValue + numButtonValue
+            }
+
+            
+        }
     
     }
 
