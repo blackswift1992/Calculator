5b11c2c17d44f98203b855fd4305537c347c97c4
Oleksii Moroz <blackswift1992@gmail.com>
2023-03-17 17:46:16 +0200

    fix dot button issue

--- Calculator/ViewController.swift
+++ Calculator/ViewController.swift
@@ -10,28 +10,50 @@ class ViewController: UIViewController {
     @IBAction private func calcButtonPressed(_ sender: UIButton) {
         isFinishedTypingNumber = true
         
-        guard let displayLabelValue = displayLabel.text,
-              let number = Double(displayLabelValue),
-              let culcButtonValue = sender.currentTitle else {
-            fatalError("Cannot convert display label text or sender current title")
+        guard let displayStringValue = displayLabel.text,
+              let displayDoubleNumber = Double(displayStringValue) else {
+            fatalError("Cannot convert display label text into Double")
+        }
+        
+        guard let culcButtonStringValue = sender.currentTitle else {
+            fatalError("Cannot convert sender current title into String")
         }
 
-        switch culcButtonValue {
+        switch culcButtonStringValue {
             case "AC": displayLabel.text = "0"
-            case "+/-": displayLabel.text = String(number * -1)
-            case "%": displayLabel.text = String(number / 100)
+            case "+/-": displayLabel.text = String(displayDoubleNumber * -1)
+            case "%": displayLabel.text = String(displayDoubleNumber / 100)
             default: return
         }
     }
     
     @IBAction private func numButtonPressed(_ sender: UIButton) {
-        if let numButtonValue = sender.currentTitle,
-           let displayLabelValue = displayLabel.text {
-            if isFinishedTypingNumber {
-                displayLabel.text = numButtonValue
-                isFinishedTypingNumber = false
+        guard let displayStringValue = displayLabel.text,
+              let displayDoubleNumber = Double(displayStringValue) else {
+            fatalError("Cannot convert display label text into Double")
+        }
+        
+        guard let numButtonStringValue = sender.currentTitle else {
+            fatalError("Cannot convert sender current title into String")
+        }
+        
+        if isFinishedTypingNumber {
+            displayLabel.text = numButtonStringValue
+            isFinishedTypingNumber = false
+            
+            if numButtonStringValue == "." {
+                displayLabel.text = "0" + numButtonStringValue
             } else {
-                displayLabel.text = displayLabelValue + numButtonValue
+                displayLabel.text = numButtonStringValue
+            }
+        } else {
+            if numButtonStringValue == "." {
+                if !displayStringValue.contains(".") {
+                    displayLabel.text = displayStringValue + numButtonStringValue
+                }
+
+            } else {
+                displayLabel.text = displayStringValue + numButtonStringValue
             }
         }
     }
