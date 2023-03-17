501a00b5142b6cfec70b0085072014393a8d06ff
Oleksii Moroz <blackswift1992@gmail.com>
2023-03-17 16:59:43 +0200

    add functionality to AC, +/-, % buttons

--- Calculator/ViewController.swift
+++ Calculator/ViewController.swift
@@ -2,19 +2,29 @@ import UIKit
 
 
 class ViewController: UIViewController {
+    @IBOutlet private weak var displayLabel: UILabel!
     
-    @IBOutlet weak var displayLabel: UILabel!
-    
-    var isFinishedTypingNumber = true
+    private var isFinishedTypingNumber = true
     
     
-    @IBAction func calcButtonPressed(_ sender: UIButton) {
+    @IBAction private func calcButtonPressed(_ sender: UIButton) {
         isFinishedTypingNumber = true
-    
-    }
+        
+        guard let displayLabelValue = displayLabel.text,
+              let number = Double(displayLabelValue),
+              let culcButtonValue = sender.currentTitle else {
+            fatalError("Cannot convert display label text or sender current title")
+        }
 
+        switch culcButtonValue {
+            case "AC": displayLabel.text = "0"
+            case "+/-": displayLabel.text = String(number * -1)
+            case "%": displayLabel.text = String(number / 100)
+            default: return
+        }
+    }
     
-    @IBAction func numButtonPressed(_ sender: UIButton) {
+    @IBAction private func numButtonPressed(_ sender: UIButton) {
         if let numButtonValue = sender.currentTitle,
            let displayLabelValue = displayLabel.text {
             if isFinishedTypingNumber {
@@ -23,11 +33,7 @@ class ViewController: UIViewController {
             } else {
                 displayLabel.text = displayLabelValue + numButtonValue
             }
-
-            
         }
-    
     }
-
 }
 
