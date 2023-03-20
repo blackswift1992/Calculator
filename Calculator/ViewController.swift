f3bf24717d9bf18e6f162ed6854344dda8fec1ac
Oleksii Moroz <blackswift1992@gmail.com>
2023-03-20 17:20:50 +0200

    refactor with displayNumber computed property

--- Calculator/ViewController.swift
+++ Calculator/ViewController.swift
@@ -6,47 +6,52 @@ class ViewController: UIViewController {
     
     private var isFinishedTypingNumber = true
     
+    private var displayNumber: Double {
+        get {
+            guard let displayStringValue = displayLabel.text,
+                  let displayNumber = Double(displayStringValue) else {
+                fatalError("Cannot convert display label text into Double")
+            }
+            
+            return displayNumber
+        }
+        
+        set {
+            displayLabel.text = String(newValue)
+        }
+    }
     
     @IBAction private func calcButtonPressed(_ sender: UIButton) {
         isFinishedTypingNumber = true
         
-        guard let displayStringValue = displayLabel.text,
-              let displayDoubleNumber = Double(displayStringValue) else {
-            fatalError("Cannot convert display label text into Double")
-        }
-        
-        guard let culcButtonStringValue = sender.currentTitle else {
+        guard let safeCulcButtonTitle = sender.currentTitle else {
             fatalError("Cannot convert sender current title into String")
         }
 
-        switch culcButtonStringValue {
+        switch safeCulcButtonTitle {
             case "AC": displayLabel.text = "0"
-            case "+/-": displayLabel.text = String(displayDoubleNumber * -1)
-            case "%": displayLabel.text = String(displayDoubleNumber / 100)
+            case "+/-": displayNumber *= -1
+            case "%": displayNumber /= 100
             default: return
         }
     }
     
     @IBAction private func numButtonPressed(_ sender: UIButton) {
-        guard let displayStringValue = displayLabel.text,
-              let displayDoubleNumber = Double(displayStringValue) else {
-            fatalError("Cannot convert display label text into Double")
-        }
-        
-        guard let numButtonStringValue = sender.currentTitle else {
-            fatalError("Cannot convert sender current title into String")
+        guard let safeNumButtonTitle = sender.currentTitle,
+              let safeDisplayText = displayLabel.text else {
+            fatalError("Cannot convert into String")
         }
         
         if isFinishedTypingNumber { //first typed digit of new number
             isFinishedTypingNumber = false
-            displayLabel.text = numButtonStringValue == "." ? "0." : numButtonStringValue
+            displayLabel.text = safeNumButtonTitle == "." ? "0." : safeNumButtonTitle
         } else {
-            if numButtonStringValue == "." {
-                if !displayStringValue.contains(".") {
-                    displayLabel.text = displayStringValue + numButtonStringValue
+            if safeNumButtonTitle == "." {
+                if !safeDisplayText.contains(".") {
+                    displayLabel.text = safeDisplayText + safeNumButtonTitle
                 }
             } else {
-                displayLabel.text = displayStringValue == "0" ? numButtonStringValue : displayStringValue + numButtonStringValue
+                displayLabel.text = safeDisplayText == "0" ? safeNumButtonTitle : safeDisplayText + safeNumButtonTitle
             }
         }
     }
